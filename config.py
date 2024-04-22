36a05bc3d096cb5609b1d25ad33e35d33a268c8c
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-04-22 15:03:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("24089031"))
+API_HASH = getenv("0615e3afe13ddaaf8e9ddbd3977d35ff")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("7028288342:AAGO5QHFzY4jG0aRpnq3SF9r4IOpBe76qr0")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 6584789596))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Flex_Support_Chat")
